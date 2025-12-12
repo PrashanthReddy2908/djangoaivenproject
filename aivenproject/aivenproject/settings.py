76057cd63327bec2493d81035cb0e454fc6b0f3d
Prashanth Kanekanti <kanakantiprashanth2@gmail.com>
2025-12-12 14:27:00 +0530

    secreate key added

--- aivenproject/aivenproject/settings.py
+++ aivenproject/aivenproject/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY ='django-insecure-!@#z9k&lqv1u$e3m0h7y5j2w4r8t6g1b3n@c%v^x&zq$w8e1r0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
